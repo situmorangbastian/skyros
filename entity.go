49e826af20af390c836472f5a7b6797e175d9baa
Bastian Situmorang <bastianpaskal@gmail.com>
2021-07-30 10:24:59 +0700

    feat: add user and product entity (#1)

---
+++ entity.go
@@ -0,0 +1,41 @@
+package skyros
+
+import (
+	"encoding/json"
+	"time"
+)
+
+type User struct {
+	ID       string `json:"-"`
+	Email    string `json:"email"`
+	Name     string `json:"name"`
+	Address  string `json:"address"`
+	Password string `json:"password"`
+	Type     string `json:"-"`
+}
+
+func (u User) MarshalJSON() ([]byte, error) {
+	return json.Marshal(&struct {
+		ID      string `json:"id"`
+		Email   string `json:"email"`
+		Name    string `json:"name"`
+		Address string `json:"address"`
+		Type    string `json:"type"`
+	}{
+		ID:      u.ID,
+		Email:   u.Email,
+		Name:    u.Name,
+		Address: u.Address,
+		Type:    u.Type,
+	})
+}
+
+type Product struct {
+	ID          string    `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Price       int64     `json:"price"`
+	Seller      User      `json:"seller"`
+	CreatedTime time.Time `json:"created_time"`
+	UpdatedTime time.Time `json:"updated_time"`
+}
