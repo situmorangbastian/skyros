2846905dd1a5372735f870d9e8550537e3b31439
Bastian Situmorang <bastianpaskal@gmail.com>
2021-07-30 21:36:05 +0700

    feat: add product interface (#13)

--- skyros.go
+++ skyros.go
@@ -11,3 +11,15 @@ type UserRepository interface {
 	Register(ctx context.Context, user User) (User, error)
 	GetUserByEmail(ctx context.Context, email string) (User, error)
 }
+
+type ProductService interface {
+	Store(ctx context.Context, product Product) (Product, error)
+	Get(ctx context.Context, ID string) (Product, error)
+	Fetch(ctx context.Context, filter Filter) ([]Product, string, error)
+}
+
+type ProductRepository interface {
+	Store(ctx context.Context, product Product) (Product, error)
+	Get(ctx context.Context, ID string) (Product, error)
+	Fetch(ctx context.Context, filter Filter) ([]Product, string, error)
+}
