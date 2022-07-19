e410a8bfdd09882f4655e6fe30421494a5906b9a
Bastian Situmorang <bastianpaskal@gmail.com>
2022-07-19 19:43:26 +0700

    feat: add product service (#67)

--- productservice/internal/mysql/migrations/20210802203821_insert_user_data.up.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `user` (`id`, `name`, `email`, `password`, `address`, `type`, `created_time`, `updated_time`, `deleted_time`)
-VALUES
-	('28e54042-71c6-444d-973d-8d2cfae157d9', 'User Seller', 'user-seller@example.com', '$2a$10$IH0ye.qEF9uhRDMi5yxQDOabW0JoJ3SHpBndCEJMc/cSZkTbFBUe.', 'Jakarta', 'seller', '2021-08-02 19:49:55', '2021-08-02 19:49:55', NULL),
-	('5f61f03c-925a-431a-a926-740472ecde5b', 'User Buyer', 'user-buyer@example.com', '$2a$10$Uh.yENEmp7WvqrHdlNbYO.zTrzdAGRyyzrSfwXXo6cIY0AHo8CfB2', 'Jakarta', 'buyer', '2021-08-02 19:51:13', '2021-08-02 19:51:13', NULL);
