f1209b58a57fb0459f2e7a07e85d437edd836f54
Bastian Situmorang <bastianpaskal@gmail.com>
2024-09-21 12:54:29 +0700

    chore: add configureable for address api docs (#117)

--- docs/main.go
+++ docs/main.go
@@ -1,18 +1,38 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/swagger"
+	"github.com/spf13/viper"
 )
 
+func init() {
+	configFile := "config.toml"
+
+	viper.SetConfigFile(configFile)
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+}
+
 func main() {
+	address := viper.GetString("server.address")
+	if address == "" {
+		log.Fatal("address is not well-set")
+	}
+
 	app := fiber.New()
 
-	app.Static("/docs", "./docs.yaml")
+	app.Static("/apidocs", "./docs.yaml")
 
-	app.Get("/swagger/*", swagger.New(swagger.Config{
-		URL: "http://localhost:8080/docs",
+	app.Get("/docs/*", swagger.New(swagger.Config{
+		URL: fmt.Sprintf("http://localhost%s/apidocs", address),
 	}))
 
-	app.Listen(":8080")
+	app.Listen(address)
 }
