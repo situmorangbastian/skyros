7cb5ed32a2555a5db3877db6cf252e92d6288a11
situmorangbastian <bastianpaskal@gmail.com>
2025-03-22 12:39:49 +0700

    chore: upgrade go version and dependency

--- productservice/api/rest/handlers/product_handler.go
+++ productservice/api/rest/handlers/product_handler.go
@@ -5,7 +5,7 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/golang-jwt/jwt"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
 
 	restCtx "github.com/situmorangbastian/skyros/productservice/api/rest/context"
